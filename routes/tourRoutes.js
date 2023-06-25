345a9a7a0f3293c52883ca214b8240678e5ebd32
SajjadFarzane <sajjadfarzane1@gmail.com>
2023-06-25 23:18:42 +0330

    - Geospatial Queries(Finding Tours Within Radius) - Geospatial Aggregation(Calculating Distances)

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -19,6 +19,12 @@ router.route('/monthly-plan/:year')
         authController.restrictTo('admin', 'lead-guide', 'guide'),
         tourController.getMonthlyPlan);
 
+router
+    .route('/tours-within/:distance/center/:latlng/unit/:unit')
+    .get(tourController.getToursWithin);
+
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
 router.route('/')
     .get(tourController.getAllTours)
     .post(
